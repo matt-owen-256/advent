435be672288e1e21ca266110685b008f8e7e3937
matt-owen-256 <matt.owen.256@pm.me>
2022-12-10 14:25:11 -0700

    refactor calc_score to use dict mapping

--- 2022/py/02_rps.py
+++ 2022/py/02_rps.py
@@ -13,32 +13,31 @@ def calc_strat():
     r_data = parse_strat()
     p_data = []
     for item in r_data:
-        item = item.replace(' ', '')
+        item = item.replace(' ','')
         ttab = str.maketrans('ABCXYZ', 'RPSRPS')
         item = item.translate(ttab)
         p_data.append(item)
     return p_data
-
 
 def calc_score():
     w_lst = ['SR', 'RP', 'PS']
     l_lst = ['RS', 'PR', 'SP']
     d_lst = ['RR', 'PP', 'SS']
 
+    score_map = {
+        'R': 1,
+        'P': 2,
+        'S': 3
+    }
+
     t_data = calc_strat()
 
     score = 0
     for item in t_data:
-        if item[1] == 'R':
-            score += 1
-        if item[1] == 'P':
-            score += 2
-        if item[1] == 'S':
-            score += 3
+        if item[1] in score_map:
+            score += score_map[item[1]]
         if item in w_lst:
             score += 6
-        if item in l_lst:
-            score += 0
         if item in d_lst:
             score += 3
     return score
@@ -46,7 +45,7 @@ def calc_score():
 
 def calc_sol():
     print(calc_score())
-
+    
 
 if __name__ == "__main__":
     calc_sol()
