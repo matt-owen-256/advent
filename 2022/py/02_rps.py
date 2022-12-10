636c4c30f6135e7a205f9c57b0f1098ea48d2d9c
matt-owen-256 <matt.owen.256@pm.me>
2022-12-10 14:30:16 -0700

    PEP-8 style cleanup

--- 2022/py/02_rps.py
+++ 2022/py/02_rps.py
@@ -13,15 +13,15 @@ def calc_strat():
     r_data = parse_strat()
     p_data = []
     for item in r_data:
-        item = item.replace(' ','')
+        item = item.replace(' ', '')
         ttab = str.maketrans('ABCXYZ', 'RPSRPS')
         item = item.translate(ttab)
         p_data.append(item)
     return p_data
 
+
 def calc_score():
     w_lst = ['SR', 'RP', 'PS']
-    l_lst = ['RS', 'PR', 'SP']
     d_lst = ['RR', 'PP', 'SS']
 
     score_map = {
