c378a0372c463971af4f381f0c189018c36c5033
matt-owen-256 <matt.owen.256@pm.me>
2022-12-22 21:05:30 -0700

    day 4 part 2 solution

--- 2022/py/04_overlap.py
+++ 2022/py/04_overlap.py
@@ -1,1 +1,31 @@
-04_overlap.py
+
+
+def parse_strat():
+    with open('../data/assignments.txt', 'r') as assign_file:
+        assigns = []
+        for line in assign_file:
+            raw_line = line.strip()
+            p_line = raw_line.replace(',', '-').split('-') 
+            p2_line = [int(item) for item in p_line]
+            assigns.append(p2_line)
+        return assigns
+
+
+def calc_range():
+    t_data = parse_strat()
+    count = 0
+    for item in t_data:
+        t0 = set(range(item[0], item[1]+1))
+        t1 = set(range(item[2], item[3]+1))
+        if t0.intersection(t1):
+            count += 1
+    return count
+
+
+def calc_sol():
+    sol = calc_range()
+    print(sol)
+
+
+if __name__ == "__main__":
+    calc_sol()
